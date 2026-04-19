56767a1e8fa3da4c3f2b1da709d9d69477e89991
Eric <eche101@UoA.auckland.ac.nz>
2026-04-19 15:56:01 +1200

    test files

--- recop/Selected VHDL source design files/execute.vhd
+++ recop/Selected VHDL source design files/execute.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 use work.recop_types.all;
 use work.various_constants.all;
 use work.opcodes.all;
@@ -11,23 +10,17 @@ entity execute is
         clk   : in bit_1;
         init  : in bit_1;
         reset : in bit_1;
-
-        -- Data inputs from decode/regfile
         rx         : in bit_16;
         rz         : in bit_16;
         ir_operand : in bit_16;
         pc_in      : in bit_16;
-
-        -- Control signals from control_signal_generator
         alu_operation : in bit_3;
         alu_op1_sel   : in bit_2;
         alu_op2_sel   : in bit_1;
         clr_z_flag    : in bit_1;
         dm_wr_en      : in bit_1;
-        dm_addr_sel   : in bit_2;   -- "00"=Rz  "01"=ir_operand  "10"=Rx
-        dm_data_sel   : in bit_2;   -- "00"=Rx  "01"=ir_operand  "10"=pc_in
-
-        -- Outputs back to DecodeandWriteback (for RF writeback)
+        dm_addr_sel   : in bit_2;
+        dm_data_sel   : in bit_2;
         alu_result : out bit_16;
         rz_max     : out bit_16;
         dm_out     : out bit_16;
@@ -36,64 +29,41 @@ entity execute is
 end execute;
 
 architecture beh of execute is
-
     signal alu_result_int : bit_16;
     signal dm_address_int : bit_16;
     signal dm_data_int    : bit_16;
-
 begin
 
-    -- MAX comparator (pure combinational)
-    -- MAX Rz #Operand: result = larger of {Rz, ir_operand}
     rz_max <= rz when (unsigned(rz) >= unsigned(ir_operand)) else ir_operand;
 
-    -- ALU (OP1/OP2 MUXes are already inside ALU.vhd)
     u_alu : entity work.alu
         port map (
-            clk           => clk,
-            z_flag        => z_flag,
+            clk => clk, z_flag => z_flag,
             alu_operation => alu_operation,
-            alu_op1_sel   => alu_op1_sel,
-            alu_op2_sel   => alu_op2_sel,
-            alu_carry     => '0',
-            alu_result    => alu_result_int,
-            rx            => rx,
-            rz            => rz,
-            ir_operand    => ir_operand,
-            clr_z_flag    => clr_z_flag,
-            reset         => reset
+            alu_op1_sel => alu_op1_sel, alu_op2_sel => alu_op2_sel,
+            alu_carry => '0', alu_result => alu_result_int,
+            rx => rx, rz => rz, ir_operand => ir_operand,
+            clr_z_flag => clr_z_flag, reset => reset
         );
 
-    -- DM Address MUX
-    -- "00" = Rz         STR Rz Rx, STR Rz #Op
-    -- "01" = ir_operand LDR Rz $Op, STR Rx $Op, STRPC $Op
-    -- "10" = Rx         LDR Rz Rx
     with dm_addr_sel select
         dm_address_int <= rz         when dm_addr_from_rz,
                           ir_operand when dm_addr_from_ir_operand,
                           rx         when dm_addr_from_rx,
                           x"0000"    when others;
 
-    -- DM Write Data MUX
-    -- "00" = Rx         STR Rz Rx, STR Rx $Op
-    -- "01" = ir_operand STR Rz #Op
-    -- "10" = pc_in      STRPC $Op
     with dm_data_sel select
         dm_data_int <= rx         when dm_data_from_rx,
                        ir_operand when dm_data_from_ir_operand,
                        pc_in      when dm_data_from_pc,
                        x"0000"    when others;
 
-    -- Data Memory (Altera altsyncram IP)
-    u_dm : entity work.data_memory_ip
+    u_dm : entity work.sim_dm
         port map (
             address => dm_address_int(11 downto 0),
-            clock   => clk,
-            data    => dm_data_int,
-            wren    => dm_wr_en,
-            q       => dm_out
+            clock => clk, data => dm_data_int,
+            wren => dm_wr_en, q => dm_out
         );
 
     alu_result <= alu_result_int;
-
 end beh;
--- recop/Selected VHDL source design files/various_constants.vhd
+++ recop/Selected VHDL source design files/various_constants.vhd
@@ -3,55 +3,32 @@ use ieee.std_logic_1164.all;
 use work.recop_types.all;
 
 package various_constants is
-
-    -- ALU operation (alu_operation : bit_3)
     constant alu_add  : bit_3 := "000";
     constant alu_sub  : bit_3 := "001";
     constant alu_and  : bit_3 := "010";
     constant alu_or   : bit_3 := "011";
     constant alu_idle : bit_3 := "100";
     constant alu_max  : bit_3 := "101";
-
-    -- ALU OP1 source (alu_op1_sel : bit_2)
-    -- "00"=Rx  "01"=ir_operand  "10"=0x0001  "11"=0x0000
     constant alu_op1_from_rx        : bit_2 := "00";
     constant alu_op1_from_immediate : bit_2 := "01";
-
-    -- ALU OP2 source (alu_op2_sel : bit_1)
-    -- '0'=Rx  '1'=Rz
     constant alu_op2_from_rx : bit_1 := '0';
     constant alu_op2_from_rz : bit_1 := '1';
-
-    -- Register File write data source (rf_input_sel : bit_3)
-    -- "000"=ir_operand  "011"=aluout  "100"=rz_max  "101"=sip_hold  "110"=er_temp  "111"=dm_out
     constant from_ir_operand : bit_3 := "000";
     constant from_aluout     : bit_3 := "011";
     constant from_rz_max     : bit_3 := "100";
     constant from_sip_hold   : bit_3 := "101";
     constant from_er_temp    : bit_3 := "110";
     constant from_dm_out     : bit_3 := "111";
-
-    -- Data memory write enable (dm_wr_en : bit_1)
     constant dm_read_enable  : bit_1 := '0';
     constant dm_write_enable : bit_1 := '1';
-
-    -- Data memory address source (dm_addr_sel : bit_2)
-    -- "00"=Rz  "01"=ir_operand  "10"=Rx
     constant dm_addr_from_rz         : bit_2 := "00";
     constant dm_addr_from_ir_operand : bit_2 := "01";
     constant dm_addr_from_rx         : bit_2 := "10";
-
-    -- Data memory write data source (dm_data_sel : bit_2)
-    -- "00"=Rx  "01"=ir_operand  "10"=pc_in
     constant dm_data_from_rx         : bit_2 := "00";
     constant dm_data_from_ir_operand : bit_2 := "01";
     constant dm_data_from_pc         : bit_2 := "10";
-
-    -- PC source (pc_sel : bit_2)
-    -- "00"=pc+1  "01"=ir_operand  "10"=Rx
     constant pc_sel_plus_one     : bit_2 := "00";
     constant pc_sel_from_operand : bit_2 := "01";
     constant pc_sel_from_rx      : bit_2 := "10";
-    constant pc_sel_from_zero    : bit_2 := "11";
-
+    constant pc_sel_from_zero    : bit_2 := "11";  -- used by FSM on reset
 end various_constants;
